9d0331fa4af67ec5f59694b5a9b7c4226668b099
Jordi Dalmau <jdalmaudev@gmail.com>
2024-11-16 08:32:38 +0100

    Refactor file structure and implement stats display in new JS file

--- ultimoAhorcado/js/popup.js
+++ ultimoAhorcado/js/popup.js
@@ -1,12 +1,9 @@
 window.onload = () => {
-    window.addEventListener("message", (event) => {
-      if (event.origin == "http://127.0.0.1:5501") {
-        console.log("comprobando mensaje");
-        
-        let intentos = event.data;
-        let imagenIntentos = document.getElementById("imagenIntentos");
-        imagenIntentos.src = `./img/${intentos}.jpg`;
-      }
-
-    });
-  }
+  window.addEventListener("message", (event) => {
+    if (event.origin == "http://127.0.0.1:5500") {
+      let intentos = event.data;
+      let imagenIntentos = document.getElementById("imagenIntentos");
+      imagenIntentos.src = `./img/${intentos}.jpg`;
+    }
+  });
+};
--- ultimoAhorcado/js/script.js
+++ ultimoAhorcado/js/script.js
@@ -1,17 +1,21 @@
 let popupWindow;
 
 window.onload = function () {
-    popupWindow = window.open("popup.html", "_blank", "width=400,height=400,top=200,left=400");
-    console.log(popupWindow);
-    
-    //  Si l’usuari tanca el popup amb la figura tornem directament al menú principal
-    const checkPopupClosed = setInterval(() => {
-        if (popupWindow.closed) {
-            clearInterval(checkPopupClosed); 
-            window.location.href = 'index.html';  
-        }
-    }, 200);
-}
+  popupWindow = window.open(
+    "popup.html",
+    "_blank",
+    "width=400,height=400,top=200,left=400"
+  );
+  console.log(popupWindow);
+
+  //  Si l’usuari tanca el popup amb la figura tornem directament al menú principal
+  const checkPopupClosed = setInterval(() => {
+    if (popupWindow.closed) {
+      clearInterval(checkPopupClosed);
+      window.location.href = "index.html";
+    }
+  }, 200);
+};
 
 const palabras = ["javascript", "html", "css"];
 let palabraRandom = palabras[Math.floor(Math.random() * palabras.length)];
@@ -29,137 +33,142 @@ const mensajeEstado = document.getElementById("mensajeEstado");
 const mensajeIntentos = document.getElementById("mensajeIntentos");
 const mensajeTiempo = document.getElementById("mensajeTiempo");
 
-
 function iniciarTemporizador() {
-    tiempoInicio = Date.now();
-    temporizadorIntervalo = setInterval(actualizarTiempo, 1000);
+  tiempoInicio = Date.now();
+  temporizadorIntervalo = setInterval(actualizarTiempo, 1000);
 }
 
 function detenerTemporizador() {
-    clearInterval(temporizadorIntervalo);
-    tiempoTranscurrido = (Date.now() - tiempoInicio) / 1000; // Tiempo en segundos
+  clearInterval(temporizadorIntervalo);
+  tiempoTranscurrido = (Date.now() - tiempoInicio) / 1000; // Tiempo en segundos
 }
 
 function actualizarTiempo() {
-    tiempoTranscurrido = (Date.now() - tiempoInicio) / 1000; // Tiempo en segundos
-    // Calculamos minutos y segundos
-    const minutos = Math.floor(tiempoTranscurrido / 60);
-    const segundos = Math.floor(tiempoTranscurrido % 60);
+  tiempoTranscurrido = (Date.now() - tiempoInicio) / 1000; // Tiempo en segundos
+  // Calculamos minutos y segundos
+  const minutos = Math.floor(tiempoTranscurrido / 60);
+  // El resto de la división por 60 son los segundos
+  const segundos = Math.floor(tiempoTranscurrido % 60);
 
-    // Mostramos el tiempo en formato "Minutos:Segundos"
-    mensajeTiempo.innerText = `Tiempo: ${minutos}m ${segundos}s`;
+  // Mostramos el tiempo en formato "Minutos:Segundos"
+  mensajeTiempo.innerText = `Tiempo: ${minutos}m ${segundos}s`;
 }
 
 function mostrarPalabra() {
-    let resultado = '';
-    for (let letra of palabraRandom) {
-        resultado += letrasAdivinadas.includes(letra) ? letra : '_';
-        resultado += ' ';
-    }
-    palabraDiv.innerHTML = resultado.trim(); //elimina espacios en blanco al final
+  let resultado = "";
+  for (let letra of palabraRandom) {
+    resultado += letrasAdivinadas.includes(letra) ? letra : "_";
+    resultado += " ";
+  }
+  palabraDiv.innerHTML = resultado.trim(); //elimina espacios en blanco al final
+}
+
+function volverIndex() {
+  // Crear el botón de volver al index
+  const botonVolver = document.createElement("button");
+  botonVolver.classList = "volver";
+  botonVolver.innerText = "Volver al inicio";
+  botonVolver.onclick = function () {
+    popupWindow.close();
+    window.location.href = "index.html";
+  };
+
+  // Añadir el botón al mensaje de estado
+  mensajeEstado.appendChild(botonVolver);
 }
 
 function estadoAhorcado() {
-    let todasLetrasAdivinadas = true;
-    for (let letter of palabraRandom) {
-        if (!letrasAdivinadas.includes(letter)) {
-            todasLetrasAdivinadas = false;
-            break;
-        }
+  let todasLetrasAdivinadas = true;
+  for (let letter of palabraRandom) {
+    if (!letrasAdivinadas.includes(letter)) {
+      todasLetrasAdivinadas = false;
+      break;
+    }
+  }
+
+  if (todasLetrasAdivinadas) {
+    detenerTemporizador();
+    mensajeEstado.innerText = `¡Ganaste! Tiempo: ${tiempoTranscurrido.toFixed(
+      1
+    )} segundos`;
+
+    volverIndex();
+
+    let estadisticasPartidaGanada = {
+      resultado: "Ganaste",
+      tiempo: tiempoTranscurrido.toFixed(1),
+      fecha: new Date().toLocaleString(),
+    };
+
+    if (localStorage.getItem("estadisticasPartida") !== null) {
+      estadisticasPartidas = JSON.parse(
+        localStorage.getItem("estadisticasPartida")
+      );
     }
 
-    if (todasLetrasAdivinadas) {
-        detenerTemporizador();
-        mensajeEstado.innerText = `¡Ganaste! Tiempo: ${tiempoTranscurrido.toFixed(1)} segundos`;
+    estadisticasPartidas.push(estadisticasPartidaGanada);
+    localStorage.setItem(
+      "estadisticasPartida",
+      JSON.stringify(estadisticasPartidas)
+    );
+    adivinaBoton.disabled = true;
+  } else if (intentos <= 1) {
+    detenerTemporizador();
+    mensajeEstado.innerText = `Perdiste. La palabra era: ${palabraRandom}. Tiempo: ${tiempoTranscurrido.toFixed(
+      1
+    )} segundos`;
 
-        // Crear el botó de tornar a l'índex
-        const botonVolver = document.createElement('button');
-        botonVolver.classList = "volver";
-        botonVolver.innerText = 'Volver al inicio';
-        botonVolver.onclick = function() {
-            popupWindow.close();
-            window.location.href = 'index.html';
-        };
+    volverIndex();
 
-        // Afegir el botó al missatge d'estat o a un altre element del DOM
-        mensajeEstado.appendChild(botonVolver);
-
-        let estadisticasPartidaGanada = {
-            resultado: "Ganaste",
-            tiempo: tiempoTranscurrido.toFixed(1),
-            fecha: new Date().toLocaleString()
-        }
-
-        if (localStorage.getItem("estadisticasPartida") !== null) {
-            estadisticasPartidas = JSON.parse(localStorage.getItem("estadisticasPartida"));
-        }
-
-        estadisticasPartidas.push(estadisticasPartidaGanada);
-        localStorage.setItem("estadisticasPartida", JSON.stringify(estadisticasPartidas));
-        adivinaBoton.disabled = true;
-
-    } else if (intentos <= 1) {
-        detenerTemporizador();
-        mensajeEstado.innerText = `Perdiste. La palabra era: ${palabraRandom}. Tiempo: ${tiempoTranscurrido.toFixed(1)} segundos`;
-
-        // Crear el botó de tornar a l'índex
-        const botonVolver = document.createElement('button');
-        botonVolver.classList = "volver";
-        botonVolver.innerText = 'Volver al inicio';
-        botonVolver.onclick = function() {
-            popupWindow.close();
-            window.location.href = 'index.html';
-        };
-
-        // Afegir el botó al missatge d'estat o a un altre element del DOM
-        mensajeEstado.appendChild(botonVolver);
-
-        let estadisticasPartidaPerdida = {
-            resultado: "Perdiste",
-            tiempo: tiempoTranscurrido.toFixed(1),
-            fecha: new Date().toLocaleString()
-        }
-        if (localStorage.getItem("estadisticasPartida") !== null) {
-            estadisticasPartidas = JSON.parse(localStorage.getItem("estadisticasPartida"));
-        }
-        estadisticasPartidas.push(estadisticasPartidaPerdida);
-        localStorage.setItem("estadisticasPartida", JSON.stringify(estadisticasPartidas));
-        adivinaBoton.disabled = true;
+    let estadisticasPartidaPerdida = {
+      resultado: "Perdiste",
+      tiempo: tiempoTranscurrido.toFixed(1),
+      fecha: new Date().toLocaleString(),
+    };
+    if (localStorage.getItem("estadisticasPartida") !== null) {
+      estadisticasPartidas = JSON.parse(
+        localStorage.getItem("estadisticasPartida")
+      );
     }
+    estadisticasPartidas.push(estadisticasPartidaPerdida);
+    localStorage.setItem(
+      "estadisticasPartida",
+      JSON.stringify(estadisticasPartidas)
+    );
+    adivinaBoton.disabled = true;
+  }
 }
 
-
-let totalcaracteres = "&,',+,-,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,²,³,·,À,Á,Å,Æ,Ç,È,É,Í,Ñ,Ò,Ó,Ö,Ø,Ú,à,á,â,ã,ä,å,ç,è,é,ê,ë,ì,í,î,ï,ñ,ò,ó,ô,ö,ø,ù,ú,û,ü,ý,Ā,ā,ă,ć,Č,č,Ē,ē,ė,ę,ī,ı,ķ,Ł,ł,ń,ň,Ō,ō,ŏ,ő,œ,ř,Ś,ś,Ş,ş,Š,š,ţ,ū,ŭ,ů,ź,ż,Ž,ž,ǎ,ǒ,Ǧ,ǧ,ḍ,ḗ,Ḥ,ḥ,ḫ,ṇ,ṛ,ṣ,Ṭ,ṭ,₂";
+let totalcaracteres =
+  "&,',+,-,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,²,³,·,À,Á,Å,Æ,Ç,È,É,Í,Ñ,Ò,Ó,Ö,Ø,Ú,à,á,â,ã,ä,å,ç,è,é,ê,ë,ì,í,î,ï,ñ,ò,ó,ô,ö,ø,ù,ú,û,ü,ý,Ā,ā,ă,ć,Č,č,Ē,ē,ė,ę,ī,ı,ķ,Ł,ł,ń,ň,Ō,ō,ŏ,ő,œ,ř,Ś,ś,Ş,ş,Š,š,ţ,ū,ŭ,ů,ź,ż,Ž,ž,ǎ,ǒ,Ǧ,ǧ,ḍ,ḗ,Ḥ,ḥ,ḫ,ṇ,ṛ,ṣ,Ṭ,ṭ,₂";
 
 let arrayCaracteres = totalcaracteres.split(",");
 
 adivinaBoton.addEventListener("click", () => {
-    const letra = letraInput.value.toLowerCase();
+  const letra = letraInput.value.toLowerCase();
 
-    if (!arrayCaracteres.includes(letra)) {
-        mensajeEstado.innerText = "Letra no válida.";
-        return;
-    }
+  if (!arrayCaracteres.includes(letra)) {
+    mensajeEstado.innerText = "Letra no válida.";
+    return;
+  }
 
-    letraInput.value = '';
+  letraInput.value = "";
 
-    if (letrasAdivinadas.includes(letra) || letra === '') {
-        mensajeEstado.innerText = "Ya adivinaste esa letra o no ingresaste nada.";
-        return;
-    }
+  if (letrasAdivinadas.includes(letra) || letra === "") {
+    mensajeEstado.innerText = "Ya adivinaste esa letra o no ingresaste nada.";
+    return;
+  }
 
-    letrasAdivinadas.push(letra);
+  letrasAdivinadas.push(letra);
 
-    if (!palabraRandom.includes(letra)) {
-        intentos--;
-        mensajeIntentos.innerText = `Intentos restantes: ${intentos - 1}`;
-        popupWindow.postMessage(intentos, "http://127.0.0.1:5501");
-    }
-    mostrarPalabra();
-    estadoAhorcado();
-}
-
-);
+  if (!palabraRandom.includes(letra)) {
+    intentos--;
+    mensajeIntentos.innerText = `Intentos restantes: ${intentos - 1}`;
+    popupWindow.postMessage(intentos, "http://127.0.0.1:5500");
+  }
+  mostrarPalabra();
+  estadoAhorcado();
+});
 
 iniciarTemporizador();
 mostrarPalabra();
